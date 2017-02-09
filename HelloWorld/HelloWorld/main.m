554966545a3311b8651a8eea054afcd4c7d23c02
atlas0904 <u920243@gmail.com>
2017-02-09 22:34:12 +0800

    1st Object C project

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by atlas on 2017/2/9.
+//  Copyright © 2017年 atlas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"I am Atlas, the first Object C project");
+    }
+    return 0;
+}
