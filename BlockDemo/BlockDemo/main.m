b255a5a8c45dfb006733de69fbb2349a57ffdfd2
atlas0904 <u920243@gmail.com>
2017-02-18 16:24:01 +0800

    Update Block example

---
+++ BlockDemo/BlockDemo/main.m
@@ -0,0 +1,33 @@
+//
+//  main.m
+//  BlockDemo
+//
+//  Created by atlas on 2017/2/18.
+//  Copyright © 2017年 atlas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//int calculateTotalPrice(int count) {
+//    int unitPrice = 10;
+//    return unitPrice * count;
+//}
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        // 1. define block var
+        int (^calculateTotalPrice) (int) = ^(int count) {
+            int unitPrice = 10;
+            return unitPrice * count;
+        };
+        
+        // 3. Use
+        int totalPrice = calculateTotalPrice(5);
+        NSLog(@"Total price: %d", totalPrice);
+        NSLog(@"Total price: %i", totalPrice);
+        
+    }
+    return 0;
+}
